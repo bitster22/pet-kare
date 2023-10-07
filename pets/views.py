7b24d18fe41b2b99f750d5ad44420d504321d681
bitster22 <57819533+bitster22@users.noreply.github.com>
2023-10-06 21:06:01 -0300

    fix: adicionando created_at na model Trait

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, status, Request, Response
+from .models import Pet
+from .serializers import PetSerializer
+from groups.serializers import GroupSerializer
+from traits.models import Trait
+from groups.models import Group
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+
+class PetView(APIView, PageNumberPagination):
+    def post(self, req: Request) -> Response:
+        serializer = PetSerializer(data=req.data)
+        serializer.is_valid(raise_exception=True)
+        traits = serializer.validated_data.pop("traits")
+
+        pet = Pet.objects.create(**serializer._validated_data)
+
+        for trait_data in traits:
+            try:
+                trait = Trait.objects.get(trait_name__iexact=trait_data["name"])
+            except Trait.DoesNotExist:
+                trait = Trait.objects.create(**trait_data)
+            pet.traits.add(trait)
+
+        group = GroupSerializer(data=req.data.group).validated_data
+
+        try:
+            group = Group.objects.get(scientific_name__exact=group["scientific_name"])
+        except Group.DoesNotExist:
+            group = Group.objects.create(**group)
+        pet.group.add(group)
+
+        serializer = PetSerializer(pet)
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+    def get(self, req: Request):
+        pets = Pet.objects.all()
+        result_page = self.paginate_queryset(pets, req, view=self)
+        serializer = PetSerializer(result_page, many=True)
+
+        return self.get_paginated_response(serializer.data)
--- traits/models.py
+++ traits/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Trait(models.Model):
     name = models.CharField(max_length=20)
-
+    created_at = models.DateTimeField(auto_now=True)
 
 # Create your models here.
