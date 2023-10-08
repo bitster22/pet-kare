1779fd30fa8492b4599fa9a11a8fe81e1d6e1d19
bitster22 <57819533+bitster22@users.noreply.github.com>
2023-10-07 23:26:36 -0300

    feat: Tarefa 3 | Rotas e Filtros finalizada

--- pets/models.py
+++ pets/models.py
@@ -14,10 +14,10 @@ class Pet(models.Model):
     sex = models.CharField(
         max_length=20, choices=SexPet.choices, default=SexPet.NOT_INFORMED
     )
-    group = models.ForeignKey(
-        "groups.Group", on_delete=models.PROTECT, related_name="pets"
-    )
     traits = models.ManyToManyField("traits.Trait", related_name="pets")
+    group = models.ForeignKey(
+        "groups.Group", on_delete=models.PROTECT, related_name="pets", null=True
+    )
 
 
 # Create your models here.
--- pets/views.py
+++ pets/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView, status, Request, Response
 from .models import Pet
 from .serializers import PetSerializer
-from groups.serializers import GroupSerializer
+
+# from groups.serializers import GroupSerializer
 from traits.models import Trait
 from groups.models import Group
 from rest_framework.pagination import PageNumberPagination
@@ -12,23 +13,22 @@ class PetView(APIView, PageNumberPagination):
         serializer = PetSerializer(data=req.data)
         serializer.is_valid(raise_exception=True)
         traits = serializer.validated_data.pop("traits")
+        group = serializer.validated_data.pop("group")
 
-        pet = Pet.objects.create(**serializer._validated_data)
+        pet = Pet.objects.create(**serializer.validated_data)
+
+        try:
+            group_data = Group.objects.get(scientific_name=group["scientific_name"])
+        except Group.DoesNotExist:
+            group_data = Group.objects.create(**group)
+        pet.group = group_data
 
         for trait_data in traits:
             try:
-                trait = Trait.objects.get(trait_name__iexact=trait_data["name"])
+                trait = Trait.objects.get(name__iexact=trait_data["name"])
             except Trait.DoesNotExist:
                 trait = Trait.objects.create(**trait_data)
             pet.traits.add(trait)
-
-        group = GroupSerializer(data=req.data.group).validated_data
-
-        try:
-            group = Group.objects.get(scientific_name__exact=group["scientific_name"])
-        except Group.DoesNotExist:
-            group = Group.objects.create(**group)
-        pet.group.add(group)
 
         serializer = PetSerializer(pet)
         return Response(serializer.data, status.HTTP_201_CREATED)
