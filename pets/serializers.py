1fc5ae93089c11e33cb3bd5918b1f368546f5adb
bitster22 <57819533+bitster22@users.noreply.github.com>
2023-10-07 23:32:43 -0300

    fix: Alteração nos serializers para adicionar configurações em falta

--- pets/serializers.py
+++ pets/serializers.py
@@ -6,7 +6,7 @@ from groups.serializers import GroupSerializer
 
 class PetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(max_length=50)
     age = serializers.IntegerField()
     weight = serializers.FloatField()
     sex = serializers.ChoiceField(choices=SexPet.choices, default=SexPet.NOT_INFORMED)
